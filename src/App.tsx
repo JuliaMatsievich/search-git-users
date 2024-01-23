1c96236f4e4646c165d01921a8b7171f906c1fa9
JuliaMatsievich <juliamatsievich@mail.ru>
2024-01-23 16:10:48 +0500

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 const App = () => {
-  return <div className="App">APP</div>;
+  return (
+    <>
+      <div className="App">APP</div>;<p>Header</p>
+    </>
+  );
 };
 
 export default App;
