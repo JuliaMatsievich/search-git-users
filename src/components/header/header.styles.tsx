848ef832125e1d1ec831cc0f8d15d933942cc34b
JuliaMatsievich <juliamatsievich@mail.ru>
2024-01-24 15:43:16 +0500

    add: logo

--- src/components/header/header.styles.tsx
+++ src/components/header/header.styles.tsx
@@ -11,7 +11,17 @@ export const HeaderContainer = styled.div`
   ${centralContainer}
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-start;
   height: 50px;
   color: #fff;
+`;
+
+export const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 20px;
+`;
+
+export const Title = styled.div`
+  font-size: 24px;
 `;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,15 @@
 import * as S from './header.styles';
 import * as CS from '../../styles/commonStyles.styles';
 
-export const Header = () => {
+export const Header: React.FC = () => {
   return (
     <>
       <S.HeaderHeader>
         <CS.Container>
-          <S.HeaderContainer>GitHub Search Users</S.HeaderContainer>
+          <S.HeaderContainer>
+            <S.Img src="\img\github-logo.png"></S.Img>
+            <S.Title>GitHub Search Users</S.Title>
+          </S.HeaderContainer>
         </CS.Container>
       </S.HeaderHeader>
     </>
